a006f3825eb8f93056d26356df480efe0287f23f
Wojciech Jaskula <w.jaskula8@gmail.com>
2024-12-11 10:52:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <p>
+        Aplikacja przeznaczona do ułatwienia warzenia piwa
+        <p>10.49</p>
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'My PWA',
+        short_name: 'PWA',
+        description: 'My Progressive Web App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true, // Włącz Service Workera w trybie dev
+        type: 'module', // Ustaw typ modułu
+      }
+    })
+  ]
+});
