2e1c85854cfb53ab5b05534dc8c7a28a1aba8f3a
wojciech.t.jaskula <wojciech.t.jaskula@gmail.com>
2025-01-10 11:59:25 +0000

    kolejne poprawki ibu

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1nkckua0h3o"
+    "revision": "0.k14rvlf6bh8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
