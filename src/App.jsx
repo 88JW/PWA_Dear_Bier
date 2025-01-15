1db5b1b9ff888acf3e03075d5982bc62590a0a44
w.jaskula8 <w.jaskula8@gmail.com>
2025-01-15 14:31:15 +0000

    zmniejszenie ikon i wersja

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ import Ideas from "./Ideas";
 
 function Home() {
   const navigate = useNavigate();
-  const [wersjaAplikacji, setWersjaAplikacji] = useState("v1.7.7");
+  const [wersjaAplikacji, setWersjaAplikacji] = useState("v1.7.8");
 
   return (
     <div className="app-container">
