d6726d421e92efa6cf3c5b289974916e602fce14
w.jaskula8 <w.jaskula8@gmail.com>
2025-01-18 19:03:39 +0000

    fix butelkowania

--- src/AsystentButelkowania.jsx
+++ src/AsystentButelkowania.jsx
@@ -12,68 +12,71 @@ function AsystentButelkowania() {
     {
       id: 1,
       title: 'Przygotowanie sprzętu',
-      description: 'Przygotuj: czyste butelki, kapsle, kapslownicę, rurkę do przelewania, miarkę do cukru, garnek, środek do dezynfekcji. Upewnij się, że wszystko jest w zasięgu ręki.',
+      description: 'Przygotuj: czyste butelki, kapsle, kapslownicę, rurkę do przelewania, miarkę do cukru, garnek, środek do dezynfekcji. Upewnij się, że wszystko jest w zasięgu ręki. Zbiornik do przelowania z kranikiem.',
       isCompleted: false,
       isOpen: true,
     },
     {
       id: 2,
-      title: 'Dezynfekcja sprzętu',
-      description: 'Wszystkie elementy mające kontakt z piwem (butelki, kapsle, rurka, kapslownica) zdezynfekuj. Zanurz je w roztworze dezynfekującym i postępuj zgodnie z instrukcją producenta.',
+      title: 'Przygotowanie roztworu dezynfekującego',
+      description: 'Zagotuj dużą ilość wody 1.5-2 litrów powinno wystarczyć. Przeczytaj ile preparatu należy rozpuścić w jednym litrze, zwykle jest to od 4-6g by uzyskać roztwór do dezynfekcji.',
       isCompleted: false,
-      isOpen: false,
+      isOpen: true,
     },
     {
       id: 3,
-      title: 'Przygotowanie roztworu cukru',
-      description: 'W garnku zagotuj ok. 0,5 litra wody i rozpuść w niej cukier (zwykle 5-8 g/l piwa). Dokładna ilość zależy od przepisu i stylu piwa. Mieszaj, aż cukier całkowicie się rozpuści.',
+      title: 'Dezynfekcja sprzętu',
+      description: 'Wszystkie elementy mające kontakt z piwem (butelki, kapsle, rurka, kapslownica) zdezynfekuj. Butelki potraktuj roztworem do dezynekcji. Kapsle zanurz roztworze dezynfekującym. Rurkę i zbiornik do przelewania popryskaj rozdrtorem zostaw wszystko na kilka min.',
       isCompleted: false,
       isOpen: false,
     },
     {
       id: 4,
-      title: 'Przelewanie piwa do butelek',
-      description: 'Przelej piwo z fermentora do czystego, zdezynfekowanego naczynia (np. drugiego fermentora), dodając wcześniej przygotowany roztwór cukru. Delikatnie wymieszaj. Użyj zdezynfekowanej rurki, by przelać piwo do butelek. Pozostaw 2-3 cm wolnej przestrzeni od góry.',
+      title: 'Przelewanie piwa z nad osadu',
+      description: 'Przelej piwo do nowego fermentatora pozbywając się osadu. Postaraj się nienapowietrzyć go.',
+      isCompleted: false,
+      isOpen: false,
+    },    
+    {
+      id: 5,
+      title: 'Przygotowanie roztworu cukru',
+      description: 'W garnku zagotuj ok. 0,5 litra wody i rozpuść w niej cukier (ilość cukru obliczysz w kalkulatorze nagazowania). Dokładna ilość zależy od przepisu i stylu piwa. Mieszaj, aż cukier całkowicie się rozpuści. Ostudź wodę z cukrem do 25C',
       isCompleted: false,
       isOpen: false,
     },
     {
-      id: 5,
+      id: 6,
+      title: 'Przelewanie piwa do butelek',
+      description: 'Do nowego fermentatora ze zlanym piwem dodaj wodę z cukrem. Delikatnie wymieszaj. Użyj zdezynfekowanej rurki, by przelać piwo do butelek. Pozostaw 2-3 cm wolnej przestrzeni od góry.',
+      isCompleted: false,
+      isOpen: false,
+    },
+    {
+      id: 7,
       title: 'Kapslowanie butelek',
       description: 'Nałóż na każdą butelkę zdezynfekowany kapsel. Użyj kapslownicy, aby szczelnie zamknąć butelki. Sprawdź, czy kapsle są dobrze zaciśnięte.',
       isCompleted: false,
       isOpen: false,
     },
     {
-      id: 6,
+      id: 8,
       title: 'Mycie i dezynfekcja',
       description: 'Po butelkowaniu umyj i zdezynfekuj cały sprzęt, który miał kontakt z piwem, aby był gotowy do następnego użycia.',
       isCompleted: false,
       isOpen: false,
     },
     {
-      id: 7,
-      title: 'Refermentacja w butelkach',
-      description: 'Przenieś zakapslowane butelki w ciepłe miejsce (20-25°C) na 1-2 tygodnie. W tym czasie drożdże przerobią dodany cukier na dwutlenek węgla, nasycając piwo.',
-      isCompleted: false,
-      isOpen: false,
-    },
-    {
-      id: 8,
-      title: 'Leżakowanie',
-      description: 'Po refermentacji przenieś butelki w chłodne i ciemne miejsce (10-15°C) na minimum 2-4 tygodnie. Dłuższe leżakowanie pozytywnie wpływa na klarowność i smak piwa.',
-      isCompleted: false,
-      isOpen: false,
-    },
-    {
       id: 9,
-      title: 'Degustacja',
-      description: 'Schłodź piwo w lodówce na kilka godzin. Otwórz butelkę i przelej piwo do szkła, ostrożnie, by ewentualny osad drożdży nie znalazł się w szklance. Ciesz się swoim piwem!',
+      title: 'Refermentacja w butelkach',
+      description: 'Przenieś zakapslowane butelki w ciepłe miejsce (20-25°C) od 5 do 10 dni. W tym czasie drożdże przerobią dodany cukier na dwutlenek węgla, nasycając piwo.',
       isCompleted: false,
       isOpen: false,
     },
+    
   ]);
 
+  const [progress, setProgress
+  ] = useState(0);
   const handleCompleteStep = (stepId) => {
     const updatedSteps = steps.map((step, index) => {
       if (step.id === stepId) {
@@ -115,6 +118,7 @@ function AsystentButelkowania() {
     const handlePreviousStep = () => {
     // brak akcji po wciśnięciu wstecz, opcja do rozbudowania
   }
+  
 
   return (
     <div>
